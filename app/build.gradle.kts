44baa1638f2acdee1f8d79be1c72e3b6b766873e
santanusur <santanu.sur@cleartrip.com>
2019-11-07 15:04:40 +0530

    Added kotlin dsl dependencies to build.gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,5 +1,3 @@
-import org.gradle.internal.impldep.org.junit.experimental.categories.Categories.CategoryFilter.include
-
 plugins {
     id(BuildPlugins.androidApplication)
     id(BuildPlugins.kotlinAndroid)
